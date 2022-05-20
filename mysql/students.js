ef60256fd5752e9bf1084aec60326b47e591666c
Jorge Henrique Ferreira da Silva <68034513+Jhferr@users.noreply.github.com>
2022-05-20 15:35:40 -0300

    Update "selectTestsToDo()"

--- mysql/students.js
+++ mysql/students.js
@@ -3,7 +3,23 @@ const connect = require("./connection");
 //  Retorna lista dos testes a serem feitos pelo usuario logado
 async function selectTestsToDo(userId){
     const conn = await connect();
-    return await conn.query(`SELECT user_id = ${userId};`)
+    return await conn.query(`SELECT heroku_65f5ce87b15f505.Quiz.id
+    FROM(
+        SELECT heroku_65f5ce87b15f505.Registration.idClassroom 
+        FROM heroku_65f5ce87b15f505.Registration
+        WHERE idStudent = ${userId}) AS A
+    INNER JOIN heroku_65f5ce87b15f505.Quiz
+    ON A.idClassroom = heroku_65f5ce87b15f505.Quiz.idClassroom
+    WHERE (
+        SELECT count(*)
+        FROM heroku_65f5ce87b15f505.Answer
+        WHERE heroku_65f5ce87b15f505.Answer.idQuiz = heroku_65f5ce87b15f505.Quiz.id
+        AND heroku_65f5ce87b15f505.Answer.idStudent = ${userId})
+        =
+        (
+        SELECT count(*)
+        FROM heroku_65f5ce87b15f505.Question
+        WHERE heroku_65f5ce87b15f505.Question.idQuiz = heroku_65f5ce87b15f505.Quiz.id);`)
 }
 
 //  Retorna lista dos testes feitos pelo usuario logado
