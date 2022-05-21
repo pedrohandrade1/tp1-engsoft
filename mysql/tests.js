71b8f8f63fb150281e7754f2fe6550f9e92644d3
Jorge Henrique Ferreira da Silva <68034513+Jhferr@users.noreply.github.com>
2022-05-21 13:36:27 -0300

    Update tests.js

--- mysql/tests.js
+++ mysql/tests.js
@@ -1,9 +1,11 @@
 const connect = require("./connection");
 
 //  Retorna informações sobre um teste especifico
-async function selectTestInfo (userId, testId) {
+async function selectTestInfo (testId) {
     const conn = await connect();
-    return await conn.query(`SELECT user_id = ${userId} AND test_id = ${testId};`)
+    return await conn.query(`SELECT * 
+    FROM heroku_65f5ce87b15f505.question
+    WHERE idQuiz = ${testId};`)
 }
 
 
