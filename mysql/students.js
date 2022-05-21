48945241e4298f1eaa03ad88250d77e206845919
Jorge Henrique Ferreira da Silva <68034513+Jhferr@users.noreply.github.com>
2022-05-21 12:33:50 -0300

    Update students.js

--- mysql/students.js
+++ mysql/students.js
@@ -3,13 +3,17 @@ const connect = require("./connection");
 //  Autentifica estudante 
 async function authenticateStudent (email, password) {
     const conn = await connect();
-    return await conn.query(`SELECT email = ${email} AND password = ${password};`) // Obs: retorna user_id
+    return await conn.query(`SELECT DISTINCT heroku_65f5ce87b15f505.student.id
+    FROM heroku_65f5ce87b15f505.student 
+    WHERE heroku_65f5ce87b15f505.student.email = ${email} AND heroku_65f5ce87b15f505.student.password = ${password});`) // Obs: retorna user_id
 }
 
 //  Retorna as informações pessoais do estudante logado
 async function selectStudentPersonalInfo (userId) {
     const conn = await connect();
-    return await conn.query(`SELECT user_id = ${userId};`)
+    return await conn.query(`SELECT DISTINCT heroku_65f5ce87b15f505.student.id, heroku_65f5ce87b15f505.student.firstName, heroku_65f5ce87b15f505.student.lastName, heroku_65f5ce87b15f505.student.email
+    FROM heroku_65f5ce87b15f505.student 
+    WHERE heroku_65f5ce87b15f505.student.id = ${userId};`)
 }
 
 //  Retorna lista dos testes a serem feitos pelo usuario logado
@@ -57,14 +61,18 @@ async function selectTestsDone (userId) {
 }
 
 //  Responde uma questão especifica de uma prova
-async function answerQuestion (userId, questionId, answer) {
+async function answerQuestion (userId, questionId, quizId, answer) {
     const conn = await connect();
 
     //  Verifica se questão já foi respondida 
-    const answered = await conn.query(`INSERT user_id = ${userId};`);
+    const answered = await conn.query(`SELECT COUNT(*) FROM heroku_65f5ce87b15f505.answer WHERE idQuiz = ${quizId} AND idStudent = ${userId} AND idQuestion = ${questionId};`);
     if (!answered) {
         //  Responde a questão
-        await conn.query(`INSERT user_id = ${userId};`)
+        await conn.query(`SET @@auto_increment_increment=1;`);
+        await conn.query(`SET @@auto_increment_offset=1;`);
+        await conn.query(`ALTER TABLE heroku_65f5ce87b15f505.answer AUTO_INCREMENT = 1;`);
+        await conn.query(`INSERT INTO heroku_65f5ce87b15f505.answer (idQuiz, idStudent, idQuestion, answerSelected)
+        VALUES (${quizId}, ${userId}, ${questionId}, ${answer});`);
     }
     return;
 }
