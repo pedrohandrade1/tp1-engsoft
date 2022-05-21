0ce1dfb0b8ab7eda05f7282ab1d0c2e6c22aa41f
Jorge Henrique Ferreira da Silva <68034513+Jhferr@users.noreply.github.com>
2022-05-21 12:34:05 -0300

    Update teachers.js

--- mysql/teachers.js
+++ mysql/teachers.js
@@ -9,13 +9,17 @@ const connect = require("./connection");
 //  Autentifica professor
 async function authenticateTeacher (email, password) {
     const conn = await connect();
-    return await conn.query(`SELECT email = ${email} AND password = ${password};`) // Obs: retorna user_id
+    return await conn.query(`SELECT DISTINCT heroku_65f5ce87b15f505.educator.id
+    FROM heroku_65f5ce87b15f505.educator 
+    WHERE heroku_65f5ce87b15f505.educator.email = ${email} AND heroku_65f5ce87b15f505.educator.password = ${password};`) // Obs: retorna user_id
 }
 
 //  Retorna as informações pessoais do professor logado
 async function selectTeacherPersonalInfo (userId) {
     const conn = await connect();
-    return await conn.query(`SELECT user_id = ${userId};`)
+    return await conn.query(`SELECT DISTINCT heroku_65f5ce87b15f505.educator.id, heroku_65f5ce87b15f505.educator.firstName, heroku_65f5ce87b15f505.educator.lastName, heroku_65f5ce87b15f505.educator.email
+    FROM heroku_65f5ce87b15f505.educator 
+    WHERE heroku_65f5ce87b15f505.educator.id = ${userId};`)
 }
 
 //  Cria uma nova prova
@@ -23,10 +27,10 @@ async function insertTest (userId, classId, testInfo) {
     const conn = await connect();
 
     //  Cria o teste e obtem o seu id
-    conn.query(`SET @@auto_increment_increment=1;`)
-    conn.query(`SET @@auto_increment_offset=1;`)
-    conn.query(`ALTER TABLE heroku_65f5ce87b15f505.quiz AUTO_INCREMENT = 1;`)
-    conn.query(`INSERT INTO heroku_65f5ce87b15f505.quiz (idClassroom, idEducator)
+    await conn.query(`SET @@auto_increment_increment=1;`)
+    await conn.query(`SET @@auto_increment_offset=1;`)
+    await conn.query(`ALTER TABLE heroku_65f5ce87b15f505.quiz AUTO_INCREMENT = 1;`)
+    await conn.query(`INSERT INTO heroku_65f5ce87b15f505.quiz (idClassroom, idEducator)
     VALUES (${classId}, ${userId});`)
     const testId = conn.query(`(SELECT COUNT(*) FROM heroku_65f5ce87b15f505.quiz)`)
 
@@ -42,9 +46,9 @@ async function insertQuestion (testId, questionInfo) {
     const conn = await connect();
     const { header, options, answer } = questionInfo;
     const [a, b, c, d, e] = options;
-    conn.query(`SET @@auto_increment_increment=1;`)
-    conn.query(`SET @@auto_increment_offset=1;`)
-    conn.query(`ALTER TABLE heroku_65f5ce87b15f505.question AUTO_INCREMENT = 1;`)
+    await conn.query(`SET @@auto_increment_increment=1;`)
+    await conn.query(`SET @@auto_increment_offset=1;`)
+    await conn.query(`ALTER TABLE heroku_65f5ce87b15f505.question AUTO_INCREMENT = 1;`)
     return await conn.query(`INSERT INTO heroku_65f5ce87b15f505.question (idQuiz, question, alternativeA, alternativeB, alternativeC, alternativeD, alternativeE, answerExpected)
     VALUES (${testId},${header}, ${a}, ${b}, ${c}, ${d}, ${e}, ${answer});`)
 }
