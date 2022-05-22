953bd185e1f929d3937bd61719a62c5884bea56c
magalhastudios1 <magalhastudios1@gmail.com>
2022-05-22 18:11:24 -0300

    Home students lista de testes

--- frontend/js/students.js
+++ frontend/js/students.js
@@ -1,29 +1,35 @@
-ENDPOINT = 'http://localhost:5500/'
+ENDPOINT = 'http://localhost:5500/';
 
-function req_done_tests(){
+
+function getTestsIdArray (xmlHttp) {
+    const response = xmlHttp.response;
+    return JSON.parse(response);
+  }
+
+function req_done_tests () {
     const xmlHttp = new XMLHttpRequest();
     let url = ENDPOINT + "students/tests/done/"
-    
-    xmlHttp.open( "GET", url, false); // false for synchronous request
-    xmlHttp.send( null );
+
+    xmlHttp.open("GET", url, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp;
 }
 
-function req_to_do_tests(){
+function req_to_do_tests () {
     const xmlHttp = new XMLHttpRequest();
-    let url = ENDPOINT + "students/tests/done/"
-    
-    xmlHttp.open( "GET", url, false); // false for synchronous request
-    xmlHttp.send( null );
+    let url = ENDPOINT + "students/tests/todo/"
+
+    xmlHttp.open("GET", url, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp;
 }
 
 function get_tests_id_array (xmlHttp) {
     const response = xmlHttp.response;
     return JSON.parse(response);
-  }
-  
-function init(){
+}
+
+function init () {
     const done_tests = req_done_tests();
     const test_done_id_array = getTestsIdArray(done_tests);
     const to_do_tests = req_to_do_tests();
@@ -32,14 +38,14 @@ function init(){
     add_to_do_test_cards(test_to_do_id_array);
 }
 
-function go_to_test(test_id){
+function go_to_test (test_id) {
     new_url = window.location.href.split('/')
     new_url = new_url.slice(0, new_url.length - 2).join('/')
     console.log(new_url + '/solve_test.html/' + test_id)
-    window.location = new_url + '/solve_test.html?test_id=' + test_id 
+    window.location = new_url + '/solve_test.html?test_id=' + test_id
 }
 
-function get_html_done_test_cards(test_id){
+function get_html_done_test_cards (test_id) {
     const html = `<div class="col-sm-5">
                     <div class="card bg-light mb-3">
                         <h5 class="card-header">Prova ${test_id}</h5>
@@ -55,9 +61,9 @@ function get_html_done_test_cards(test_id){
                     </div>
                 </div>`
     return html;
-  }
+}
 
-function get_html_to_do_test_cards(test_id){
+function get_html_to_do_test_cards (test_id) {
     const html = `<div class="col-sm-5">
                     <div class="card bg-light mb-3">
                         <h5 class="card-header">Prova ${test_id}</h5>
@@ -72,28 +78,28 @@ function get_html_to_do_test_cards(test_id){
                     </div>
                 </div>`
     return html;
-    }
+}
 
-function add_done_test_cards(test_id_array){
+function add_done_test_cards (test_id_array) {
     let html = "";
-    for(let i = 0; i < test_id_array.length; i++){
+    for (let i = 0; i < test_id_array.length; i++) {
         const row = test_id_array[i];
         const test_id = row.id;
-        html+= get_html_done_test_cards(test_id);
+        html += get_html_done_test_cards(test_id);
     }
     console.log(html)
     const node = document.getElementById("tests-done");
     node.innerHTML = html;
-    } 
+}
 
-function add_to_do_test_cards(test_id_array){
+function add_to_do_test_cards (test_id_array) {
     let html = "";
-    for(let i = 0; i < test_id_array.length; i++){
+    for (let i = 0; i < test_id_array.length; i++) {
         const row = test_id_array[i];
         const test_id = row.id;
-        html+= get_html_to_do_test_cards(test_id);
+        html += get_html_to_do_test_cards(test_id);
     }
     console.log(html)
     const node = document.getElementById("tests-to-do");
     node.innerHTML = html;
-    } 
+} 
--- mysql/connection.js
+++ mysql/connection.js
@@ -2,10 +2,6 @@ const mysql = require('mysql2/promise');
 
 //  Conecta ao banco de dados do Heroku
 async function connect () {
-    //  Verifica se já conectou ao banco de dados
-    if (global.connection && global.connection.state != "disconnected") {
-        return global.connection;
-    }
 
     //  Credenciais do banco
     const user = 'b1929a09a7817c';
@@ -15,7 +11,7 @@ async function connect () {
 
     //  Conexão
     const connection = await mysql.createConnection(`mysql://${user}:${password}@${adress}/${db}`);
-    global.connection = connection;
+
     return connection;
 }
 
