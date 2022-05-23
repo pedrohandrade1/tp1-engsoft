2a6d7a1cf05edd92f1acc8a48669a7988e103596
ritarez <ritarezborges@gmail.com>
2022-05-22 23:43:10 -0300

    init solve_test page

--- frontend/js/solve_test.js
+++ frontend/js/solve_test.js
@@ -6,11 +6,65 @@ function req_student_info(){
     
     xmlHttp.open( "GET", url, false); // false for synchronous request
     xmlHttp.send( null );
-    return xmlHttp;
+    return xmlHttp.response;
 }
 
-function render_user_info(){
-    ans = req_student_info()
-    print(ans)
+function req_test_info(test_id){
+    const xmlHttp = new XMLHttpRequest();
+    let url = ENDPOINT + "tests/" + test_id + "/"
     
+    xmlHttp.open( "GET", url, false); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.response;
+}
+
+function get_html_questions_cards (question) {
+    const html = `<div class="card" style="width: 40rem;">
+                    <form><div class="card-body">
+                        <h5 id="question" class="card-title">${question.question}</h5>
+                        <ul class="list-group list-group-flush">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1"> ${question.alternativeA}</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1"> ${question.alternativeB} </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1"> ${question.alternativeC} </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1"> ${question.alternativeD} </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1"> ${question.alternativeE} </label>
+                            </div>
+                        </ul>
+                    </div></form>
+                </div>`
+    return html;
+}
+
+function add_question_card(questions) {
+    let html = "";
+    for (let i = 0; i < 5; i++) {
+        html += get_html_questions_cards(questions[i]);
+    }
+    const node = document.getElementById("questions");
+    node.innerHTML = html;
+} 
+
+function render_user_info(){
+    ans = JSON.parse(req_student_info())
+    console.log(ans)
+    document.getElementById('student_name').innerHTML = ans.firstName + " " + ans.lastName
+    
+    test_id = window.location.href.split('/').slice(-1)
+    questions = JSON.parse(req_test_info(test_id)) 
+    console.log(questions)
+    add_question_card(questions)
 }
